b575c42fcc07c0df6a559ef25917d567f490221a
lexiii <leck.she@gmail.com>
2015-09-15 19:10:14 +1000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,1 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "root";
-$db_name = "timesheet5";
 
-DEFINE("SQLHost",$SQLHost);
-DEFINE("SQLUsername",$SQLUsername);
-DEFINE("SQLPassword",$SQLPassword);
-DEFINE("SQLDB",$SQLDB);
-?>
